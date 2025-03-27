25f6fa52fa762acc45ff9844cde76cab7dcd10cc
Elijah Olmos <eli.olmos9@gmail.com>
2025-03-26 21:19:05 -0700

    migrate to @emailjs/browser

--- src/app/Form.tsx
+++ src/app/Form.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import emailjs from '@emailjs/browser'
 import { Button } from '@/components/ui/button';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
@@ -31,17 +32,18 @@ export function ProfileForm() {
 
 	// 2. Define a submit handler.
 	function onSubmit(values: z.infer<typeof formSchema>) {
-		// Do something with the form values.
-		// ✅ This will be type-safe and validated.
 		console.log(values);
-		fetch('/api/form/', {
-			method: 'POST',
-			body: JSON.stringify(values),
-			headers: {
-				'Content-Type': 'application/json',
+		emailjs.init({
+			publicKey: process.env.NEXT_PUBLIC_EMAIL_PUBLIC_KEY,
+			limitRate: {
+				id: "vercel",
+				throttle: 5000,
 			},
-		})
+		});
+
+		emailjs.send("service_h6lo7jb", "template_voccwdi", values);
 	}
+
 
 	return (
 		<div className="flex min-h-screen flex-col items-center justify-center bg-[#FDFDFD] px-4">
